d975b82706bac12b6dd06a55f65dcba99cd41c63
kamja44 <88872409+kamja44@users.noreply.github.com>
2025-10-18 22:59:13 +0900

    open-next.config.ts => js

---
+++ frontend/open-next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('@opennextjs/cloudflare').OpenNextConfig} */
+const config = {
+  cloudflare: {
+    dangerousDisableConfigValidation: false,
+  },
+  default: {},
+};
+
+export default config;
