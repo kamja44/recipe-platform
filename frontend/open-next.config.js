3a276d99e7cb9f478f58b403599ff77d37613e90
kamja44 <88872409+kamja44@users.noreply.github.com>
2025-10-19 11:00:49 +0900

    claudFlare => Vercel

--- frontend/open-next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('@opennextjs/cloudflare').OpenNextConfig} */
-const config = {
-  cloudflare: {
-    dangerousDisableConfigValidation: false,
-  },
-  default: {},
-};
-
-export default config;
